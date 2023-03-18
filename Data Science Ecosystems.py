e43b40e278876529750aabe5f4a67c86b107d64f
bakkerjul <128192029+bakkerjul@users.noreply.github.com>
2023-03-18 15:23:19 -0500

    Add files via upload

---
+++ Data Science Ecosystems.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+#  In this notebook, Data Science Tools and Ecosystems are summarized
+#  Some of the popular languages that Data Scientists use are:
+#  
+#  **Objectives**
+# - Python
+# - Java
+# - C++
+# - R
+# 
+# # Data Science Tools
+# ###  Below are a few examples of evaluting arithemetic expressions in Python
+# This is a simple arithmetic expression to multiply then add integers
+# 
+# 
+# ##  Author
+# Julia Bakker
+# 
+# 
+# 
+# 
+
+# 
+
+# In[3]:
+
+
+(3*4)+5
+
+
+# In[2]:
+
+
+200/60
+
+
+# In[ ]:
+
+
+
+
